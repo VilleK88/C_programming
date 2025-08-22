92e1702deb58cb5f1b74402348ab40f6353ef2bc
ville <ville.karppinen88@gmail.com>
2025-08-22 15:16:51 +0300

    updated ex3

--- Ex3/main.c
+++ Ex3/main.c
@@ -1,90 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int handle_input(int value);
-int handle_memory(int *students, int size);
-void initialize_students(int size, int *students);
-int handle_students(int *students, int size);
+int handle_input();
+void initialize_students(int **this_array, int *this_size);
+int handle_memory(int **this_array, int this_size);
+void handle_students(int *this_array, int this_size);
+void print_students(int *this_array, int this_size);
 
 int main() {
-    int *students;
-    int size, count;
+    int *students, size;
 
-    printf("How many students: ");
-    size = handle_input(size);
-
-    handle_memory(students, size);
-
-    initialize_students(size, students);
-
+    initialize_students(&students, &size);
     handle_students(students, size);
+    print_students(students, size);
 
     free(students);
-
     return 0;
 }
 
-int handle_input(int value)
-{
-    if(scanf("%d", &value) != 1)
-    {
-        while(getchar() != '\n');
-        printf("Invalid input\n");
+int handle_input() {
+    int value;
+
+    while (1) {
+        if (scanf("%d", &value) != 1) {
+            while (getchar() != '\n');
+            printf("Invalid input.\n");
+        }
+        else break;
     }
+
     return value;
 }
 
-int handle_memory(int *students, int size)
-{
-    students = malloc(size * sizeof(int));
-    if(students == NULL)
-    {
+void initialize_students(int **this_array, int *this_size) {
+    printf("How many students: ");
+    *this_size = handle_input();
+
+    handle_memory(this_array, *this_size);
+
+    for (int i = 0; i < *this_size; i++) {
+        (*this_array)[i] = -1;
+    }
+}
+
+int handle_memory(int **this_array, int this_size) {
+    *this_array = malloc(this_size * sizeof(int));
+    if (*this_array == NULL) {
         printf("Memory allocation failed!\n");
         return 0;
     }
+    return 1;
 }
 
-void initialize_students(int size, int *students)
-{
-    for(int i = 0; i < size; i++)
-    {
-        students[i] = -1;
+void handle_students(int *this_array, int this_size) {
+    printf("This size: %d\n", this_size);
+    while (1) {
+        printf("Enter student number (1 - %d) or 0 to stop: ", this_size);
+        int num = handle_input();
+        if (num == 0)
+            break;
+        else if (num < 1 || num > this_size) {
+            printf("Invalid student number!\n");
+        }
+        else {
+            printf("Enter grade (0 - 5) for student %d or -1 to cancel: ", num);
+            int const grade = handle_input();
+            if (grade == -1)
+                continue;
+            else {
+                if (grade < 0 || grade > this_size)
+                    printf("Invalid grade!\n");
+                else {
+                    this_array[num-1] = grade;
+                    printf("Grade given to student: %d.\n", grade);
+                }
+            }
+        }
     }
 }
 
-int handle_students(int *students, int size)
-{
-    while(1)
-    {
-        int num, grade;
-
-        printf("Enter student number (1 - %d) or 0 to stop: ", size);
-        num = handle_input(num);
-
-        if(num == 0)
-        {
-            return 0;
-        }
-        else if(num < 0 || num > size)
-        {
-            printf("Invalid student number!\n");
-        }
+void print_students(int *this_array, int this_size) {
+    printf("Student   Grade\n");
+    for (int i = 0; i < this_size; i++) {
+        if (this_array[i] == -1)
+            printf("%d %11s\n", i + 1, "N/A");
         else
-        {
-            printf("Enter grade (0 - 5) for student %d or -1 to cancel: ", num);
-            grade = handle_input(grade);
-            if(grade == -1)
-            {
-                continue;
-            }
-            else if(grade >= 0 && grade <= size)
-            {
-                students[num] = grade;
-            }
-            else
-            {
-                printf("Invalid grade!\n");
-            }
-        }
+            printf("%d %9d\n", i + 1, this_array[i]);
     }
 }
---
+++ Ex8/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h> // printf, scanf, fgets
+#include <stdlib.h> // malloc, realloc, free, atoi
+#include <string.h> // strcmp, strcspn, strlen
+#include <time.h>
+
+void initialize_array(int *this_array, int count);
+void print_numbers(const int *this_array, int count);
+
+int main() {
+    int *numbers, count;
+    srand(time(NULL));
+
+
+}
+
+void initialize_array(int *this_array, int count) {
+
+}
+
+void print_numbers(const int *this_array, int count) {
+
+}
