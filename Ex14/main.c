7e4c5fd813cc8dbaa45d2e8488c56ed268b8128f
ville <ville.karppinen88@gmail.com>
2025-08-29 13:08:37 +0300

    Ex14 done

--- Ex14/main.c
+++ Ex14/main.c
@@ -1,11 +1,13 @@
-#include <stdio.h> // printf, fgets, scanf
-#include <stdlib.h> // malloc, realloc, free, atoi
-#include <string.h> // strcmp, strcspn, strlen, strstr
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 char *handle_input();
 FILE *open_file(char *this_filename);
 char *read_file(FILE *this_file, int *count_out);
-void print_array(char (*this_lines)[81], int this_count);
+void convert_to_uppercase(char (*this_lines)[81], int this_count);
+void write_to_file(char *this_filename, char (*this_lines)[81], int this_count);
 
 int main() {
     int count = 0;
@@ -13,7 +15,8 @@ int main() {
     char *filename = handle_input();
     FILE *file = open_file(filename);
     char (*lines)[81] = read_file(file, &count);
-    print_array(lines, count);
+    convert_to_uppercase(lines, count);
+    write_to_file(filename, lines, count);
 
     free(lines);
     free(filename);
@@ -68,8 +71,26 @@ char *read_file(FILE *this_file, int *count_out) {
     return (char*)lines;
 }
 
-void print_array(char (*this_lines)[81], int this_count) {
+void convert_to_uppercase(char (*this_lines)[81], int this_count) {
     for (int i = 0; i < this_count; i++) {
-        printf("%s\n", this_lines[i]);
+        for (int j = 0; j < strlen(this_lines[i]); j++) {
+            //printf("%c\n", this_lines[i][j]);
+            char c = this_lines[i][j];
+            this_lines[i][j] = toupper(c);
+        }
     }
+}
+
+
+void write_to_file(char *this_filename, char (*this_lines)[81], int this_count) {
+    FILE *file = fopen(this_filename, "w");
+    if (file == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < this_count; i++) {
+        fprintf(file, "%s\n", this_lines[i]);
+    }
+
+    fclose(file);
 }
