d514f4dc7610b1b31728ced5f6dff9eb346d03c7
ville <ville.karppinen88@gmail.com>
2025-08-30 05:10:04 +0300

    Ex12 updated

--- Ex12/main.c
+++ Ex12/main.c
@@ -32,13 +32,12 @@ int count_words(const char* str, const char *word) {
 }
 
 void handle_program(char *this_string, char *this_word) {
-    while (1) {
+    do {
         handle_input("Enter a string: ", this_string, 80);
         handle_input("Enter a word: ", this_word, 80);
-
-        if (strcmp(this_word, "stop") == 0) break;
-
-        const int sum = count_words(this_string, this_word);
-        printf("sum: %d\n", sum);
-    }
+        if (strcmp(this_word, "stop") != 0) {
+            const int sum = count_words(this_string, this_word);
+            printf("sum: %d\n", sum);
+        }
+    } while (strcmp(this_word, "stop") != 0);
 }
