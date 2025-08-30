2a03fbb2e2ccfb75c434d07f237663551b390750
ville <ville.karppinen88@gmail.com>
2025-08-30 06:05:45 +0300

    Ex5 updated

--- Ex5/main.c
+++ Ex5/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int handle_input(void);
-void handle_roll_dice(void);
-int read_range(int this_low, int this_high);
+void handle_loop(int *this_count, int *this_low, int *this_high);
+int read_range(int *this_low, int *this_high);
 
 int main()
 {
-    handle_roll_dice();
+    int low = 1, high = 10, count = 0;
 
+    printf("Let's play!\nRoll a die and enter your result.\n");
+    handle_loop(&count, &low, &high);
+    printf("Better luck next time. Bye!\n");
+    
     return 0;
 }
 
@@ -25,36 +29,30 @@ int handle_input(void)
     return value;
 }
 
-void handle_roll_dice(void)
+void handle_loop(int *this_count, int *this_low, int *this_high)
 {
-    int low = 1, high = 10, count = 0;
-
-    printf("Let's play!\nRoll a die and enter your result.\n");
-
-    while(count < 3)
+    while(*this_count < 3)
     {
-        int num = read_range(low, high);
+        int num = read_range(this_low, this_high);
         if(num != 6)
             printf("I got %d. I win!\n", num + 1);
         else
             printf("I got %d. It is a tie!\n", num);
 
-        count++;
+        (*this_count)++;
     }
-
-    printf("Better luck next time. Bye!\n");
 }
 
-int read_range(int this_low, int this_high)
+int read_range(int *this_low, int *this_high)
 {
     int value;
 
     do {
-        printf("Enter a number between %d and %d: ", this_low, this_high);
+        printf("Enter a number between %d and %d: ", (*this_low), (*this_high));
         value = handle_input();
-        if (value < this_low || value > this_high)
+        if (value < *this_low || value > *this_high)
             printf("Number was outside the specified range. Try again.\n");
-    } while (value < this_low || value > this_high);
+    } while (value < *this_low || value > *this_high);
 
     return value;
 }
