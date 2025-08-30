6cfc7dd5c66fa097cdfd9b39bb810b2645cb2d82
ville <ville.karppinen88@gmail.com>
2025-08-30 06:36:00 +0300

    Ex11 updated

--- Ex11/main.c
+++ Ex11/main.c
@@ -3,18 +3,29 @@
 
 int replace_char(char *str, const char *repl);
 void handle_input(char *description, char *this_text, int this_count);
-void handle_program(char *this_text, char *this_repl);
 void trim_newline(char *input);
 
 int main() {
     char text[80], repl[3];
-    handle_program(text, repl);
+
+    handle_input("Enter string: ", text, 80);
+    handle_input("Enter replace: ", repl, 3);
+
+    const int count = replace_char(text, repl);
+
+    if (count <= 0) {
+        printf("String was not modified.\n");
+    }
+    else {
+        printf("%d\n", count);
+        printf("%s", text);
+    }
 
     return 0;
 }
 
 int replace_char(char *str, const char *repl) {
-    int length = (int)strlen(str);
+    const int length = (int)strlen(str);
     int count = 0;
 
     if (strlen(repl) < 2) return 0;
@@ -35,21 +46,6 @@ void handle_input(char *description, char *this_text, int this_count) {
     printf("%s", description);
     fgets(this_text, this_count, stdin);
     trim_newline(this_text);
-}
-
-void handle_program(char *this_text, char *this_repl) {
-    handle_input("Enter string: ", this_text, 80);
-    handle_input("Enter replace: ", this_repl, 3);
-
-    const int count = replace_char(this_text, this_repl);
-
-    if (count <= 0) {
-        printf("String was not modified.\n");
-    }
-    else {
-        printf("%d\n", count);
-        printf("%s", this_text);
-    }
 }
 
 void trim_newline(char *input) {
