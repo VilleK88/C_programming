727cd8c41566d92d3a69f26e1b25ae2c8171dd71
ville <ville.karppinen88@gmail.com>
2025-09-02 18:41:19 +0300

    Ex16 done

--- Ex16/main.c
+++ Ex16/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct node {
     int number;
@@ -8,29 +9,36 @@ typedef struct node {
 } nnode;
 
 char *handle_input();
-struct node *add(struct node *head, int value);
+void update_list_and_array(struct node ***node_array, size_t *count, struct node **head, long val);
+struct node *add_to_llist(struct node *head, int value);
+void print_numbers(struct node *head);
+void input_warning_and_free_memory(char *description, char* string);
 
 int main() {
     nnode **node_array = NULL;
     size_t count = 0;
     char *input;
-    int value = 0;
     struct node *head = NULL;
+    bool check = true;
 
     do {
         printf("Enter integer: ");
-        scanf_s("%d", &value);
-        nnode *tmp = realloc(node_array, (count + 1) * sizeof * node_array);
-        node_array = tmp;
-        head = add(head, value);
-        node_array[count++] = head;
+        input = handle_input();
+        if (input != NULL) {
+            char *parse_end;
+            // Convert string in 'input' to a long integer in base 10.
+            // 'parse_end' will point to the first character after the parsed number.
+            // If the whole string was a number, then *parse_end == '\0'.
+            const long val = strtol(input, &parse_end, 10);
+            if (*parse_end == '\0')
+                update_list_and_array(&node_array, &count, &head, val);
+            else if((strcmp(input, "end") == 0))
+                check = false;
+        }
 
-    } while (value > 0);
+    } while (check == true);
 
-    printf("Numbers:\n");
-    for (nnode *p = head; p; p = p->next) {
-        printf("%d ", p->number);
-    }
+    print_numbers(head);
 
     free(input);
     free(node_array);
@@ -40,24 +48,36 @@ int main() {
 char *handle_input() {
     char *string = malloc(32);
     if (!string) {
-        printf("Memory allocation failed!\n");
+        input_warning_and_free_memory("Memory allocation failed.\n", string);
         return NULL;
     }
 
     printf("Enter a integer or 'end' to stop: ");
     if (!fgets(string, 32, stdin)) {
-        free(string);
+        input_warning_and_free_memory("The input reading failed (EOF or input error).\n", string);
         return NULL;
     }
 
     string[strcspn(string, "\n")] = '\0';
 
+    if (string[0] == '\0') {
+        input_warning_and_free_memory("Empty input.\n", string);
+        return NULL;
+    }
+
     return string;
 }
 
-struct node *add(struct node *head, int value) {
-    nnode *newline = malloc(sizeof(*newline));
+void update_list_and_array(struct node ***node_array, size_t *count, struct node **head, long val) {
+    int value = (int)val;
+    nnode **tmp = realloc(*node_array, (*count + 1) * sizeof * node_array);
+    *node_array = tmp;
+    *head = add_to_llist(*head, value);
+    (*node_array)[(*count)++] = *head;
+}
 
+struct node *add_to_llist(struct node *head, int value) {
+    nnode *newline = malloc(sizeof(*newline));
     newline->next = NULL;
     newline->number = value;
 
@@ -66,8 +86,19 @@ struct node *add(struct node *head, int value) {
 
     struct node *tail = head;
     while (tail->next) tail = tail->next;
-
     tail->next = newline;
 
     return head;
+}
+
+void print_numbers(struct node *head) {
+    printf("Numbers:\n");
+    for (nnode *p = head; p; p = p->next) {
+        printf("%d ", p->number);
+    }
+}
+
+void input_warning_and_free_memory(char *description, char* string) {
+    printf("%s", description);
+    free(string);
 }
--- Test/main.c
+++ Test/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct node {
     int number;
@@ -8,29 +9,34 @@ typedef struct node {
 } nnode;
 
 char *handle_input();
+void update_list_and_array(struct node ***node_array, size_t *count, struct node **head, long val);
 struct node *add_to_llist(struct node *head, int value);
 void print_numbers(struct node *head);
+void input_warning_and_free_memory(char *description, char* string);
 
 int main() {
     nnode **node_array = NULL;
     size_t count = 0;
     char *input;
     struct node *head = NULL;
+    bool check = true;
 
     do {
         printf("Enter integer: ");
         input = handle_input();
-        char *endptr;
-        long val = strtol(input, &endptr, 10);
-        if (*endptr == '\0') {
-            int value = (int)val;
-            nnode *tmp = realloc(node_array, (count + 1) * sizeof * node_array);
-            node_array = tmp;
-            head = add_to_llist(head, value);
-            node_array[count++] = head;
+        if (input != NULL) {
+            char *parse_end;
+            // Convert string in 'input' to a long integer in base 10.
+            // 'parse_end' will point to the first character after the parsed number.
+            // If the whole string was a number, then *parse_end == '\0'.
+            const long val = strtol(input, &parse_end, 10);
+            if (*parse_end == '\0')
+                update_list_and_array(&node_array, &count, &head, val);
+            else if((strcmp(input, "end") == 0))
+                check = false;
         }
 
-    } while (strcmp(input, "end") != 0);
+    } while (check == true);
 
     print_numbers(head);
 
@@ -42,19 +48,32 @@ int main() {
 char *handle_input() {
     char *string = malloc(32);
     if (!string) {
-        printf("Memory allocation failed!\n");
+        input_warning_and_free_memory("Memory allocation failed.\n", string);
         return NULL;
     }
 
     printf("Enter a integer or 'end' to stop: ");
     if (!fgets(string, 32, stdin)) {
-        free(string);
+        input_warning_and_free_memory("The input reading failed (EOF or input error).\n", string);
         return NULL;
     }
 
     string[strcspn(string, "\n")] = '\0';
 
+    if (string[0] == '\0') {
+        input_warning_and_free_memory("Empty input.\n", string);
+        return NULL;
+    }
+
     return string;
+}
+
+void update_list_and_array(struct node ***node_array, size_t *count, struct node **head, long val) {
+    int value = (int)val;
+    nnode **tmp = realloc(*node_array, (*count + 1) * sizeof * node_array);
+    *node_array = tmp;
+    *head = add_to_llist(*head, value);
+    (*node_array)[(*count)++] = *head;
 }
 
 struct node *add_to_llist(struct node *head, int value) {
@@ -77,4 +96,9 @@ void print_numbers(struct node *head) {
     for (nnode *p = head; p; p = p->next) {
         printf("%d ", p->number);
     }
+}
+
+void input_warning_and_free_memory(char *description, char* string) {
+    printf("%s", description);
+    free(string);
 }
