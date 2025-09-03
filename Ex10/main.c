0dc4bfebeb4e27c80d93fbd465ab663b88b9459d
ville <ville.karppinen88@gmail.com>
2025-09-03 06:07:55 +0300

    Ex10 updated

--- Ex10/main.c
+++ Ex10/main.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 char *handle_input();
+char *input_warning_and_free_memory(char *description, char* string);
 
 int main() {
     char *string = NULL;
+    bool check = true;
 
     do {
         printf("Enter a string: ");
         string = handle_input();
-        const size_t length = strlen(string);
-        printf("Length of the string: %zu.\n", length);
-    } while (strcmp(string, "stop") != 0);
+        if (string != NULL) {
+            const size_t length = strlen(string);
+            printf("Length of the string: %zu.\n", length);
+            if(strcmp(string, "stop") == 0)
+                check = false;
+        }
+    } while (check == true);
 
     free(string);
-
     return 0;
 }
 
 char *handle_input() {
     char *string = malloc(32);
-    if (!string) {
-        printf("Memory allocation failed\n");
-        return NULL;
-    }
+    if (!string)
+        return input_warning_and_free_memory("Memory allocation failed.\n", string);
 
-    if (!fgets(string, 32, stdin)) {
-        free(string);
-        return NULL;
-    }
+    if (!fgets(string, 32, stdin))
+        return input_warning_and_free_memory("The input reading failed (EOF or input error).\n", string);
 
     string[strcspn(string, "\n")] = '\0';
 
+    if (string[0] == '\0')
+        return input_warning_and_free_memory("Empty input.\n", string);
+
     return string;
+}
+
+char *input_warning_and_free_memory(char *description, char* string) {
+    printf("%s", description);
+    free(string);
+    return NULL;
 }
