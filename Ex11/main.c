be570644e4af9eda02d17cc6e673ad8cd9320d5c
ville <ville.karppinen88@gmail.com>
2025-09-14 15:16:33 +0300

    Ex11 updated

--- Ex11/main.c
+++ Ex11/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int replace_char(char *str, const char *repl);
-void handle_input(char *description, char *this_text, int this_count);
+bool get_input(char *user_input, int length);
+void handle_input(char *input_text, char *user_input, int length);
 
 int main() {
-    char text[32], repl[3];
+    char text[34], repl[4];
 
-    handle_input("Enter string: ", text, 32);
-    handle_input("Enter replace: ", repl, 3);
+    handle_input("Enter string: ", text, 34);
+    handle_input("Enter replace: ", repl, 4);
 
     const int count = replace_char(text, repl);
 
@@ -24,25 +26,39 @@ int main() {
 }
 
 int replace_char(char *str, const char *repl) {
-    const int length = (int)strlen(str);
-    int count = 0;
-
     if (strlen(repl) < 2) return 0;
-
-    for (int i = 0; i < length; i++) {
+    int count = 0;
+    for (int i = 0; i < (int)strlen(str); i++) {
         if (str[i] == repl[0]) {
             str[i] = repl[1];
             count++;
         }
     }
-
-    if (count <= 0) return 0;
-
     return count;
 }
 
-void handle_input(char *description, char *this_text, int this_count) {
-    printf("%s", description);
-    fgets(this_text, this_count, stdin);
-    this_text[strcspn(this_text, "\n")] = '\0';
+bool get_input(char *user_input, const int length) {
+    if(fgets(user_input, length, stdin)) {
+        if (strchr(user_input, '\n') == NULL) {
+            int c = 0;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            printf("Input too long (max %d characters).\n", length-2);
+            return false;
+        }
+        user_input[strcspn(user_input, "\n")] = '\0';
+        if (user_input[0] == '\0') {
+            printf("Empty input.\n");
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+void handle_input(char *input_text, char *user_input, const int length) {
+    bool continue_loop = false;
+    while (!continue_loop) {
+        printf("%s", input_text);
+        continue_loop = get_input(user_input, length);
+    }
 }
