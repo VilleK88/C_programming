80981217ddaa0b2e9ff3e17ae2c9dfc24ce02e7e
ville <ville.karppinen88@gmail.com>
2025-09-16 13:45:05 +0300

    Ex14 updated

--- Ex14/main.c
+++ Ex14/main.c
@@ -3,47 +3,51 @@
 #include <string.h>
 #include <ctype.h>
 
-char *handle_input();
+#define line_length 82
+#define max_lines 100
+#define filename_length 32
+
+char *get_filename();
 FILE *open_file(char *this_filename);
-char *read_file(FILE *this_file, int *count_out);
-void convert_to_uppercase(char (*this_lines)[81], int this_count);
-void write_to_file(char *this_filename, char (*this_lines)[81], int this_count);
+char (*read_file(FILE *this_file, int *count_out))[line_length];
+void convert_to_uppercase(char (*this_lines)[line_length], int this_count);
+void write_to_file(char *this_filename, char (*this_lines)[line_length], int this_count);
 
 int main() {
     int count = 0;
 
-    char *filename = handle_input();
+    char *filename = get_filename();
     FILE *file = open_file(filename);
-    char (*lines)[81] = read_file(file, &count);
+    char (*lines)[line_length] = read_file(file, &count);
     if (lines != NULL) {
         convert_to_uppercase(lines, count);
         write_to_file(filename, lines, count);
         free(lines);
     }
 
-    if (!filename) free(filename);
     return 0;
 }
 
-char *handle_input() {
-    char *string = malloc(32);
+char *get_filename() {
+    char *string = malloc(filename_length);
     if (!string) {
         printf("Memory allocation failed!\n");
+        free(string);
         return NULL;
     }
 
     printf("Enter a filename: ");
-    if (!fgets(string, 32, stdin)) {
+    if (!fgets(string, filename_length, stdin)) {
         free(string);
         return NULL;
     }
+
     string[strcspn(string, "\n")] = '\0';
     return string;
 }
 
 FILE *open_file(char *this_filename) {
     FILE *file;
-
     if ((file = fopen(this_filename, "r")) == NULL) {
         fprintf(stderr, "Error: could not open file: '%s'\n", this_filename);
         free(this_filename);
@@ -52,28 +56,27 @@ FILE *open_file(char *this_filename) {
     return file;
 }
 
-char *read_file(FILE *this_file, int *count_out) {
+char (*read_file(FILE *this_file, int *count_out))[line_length] {
     int count = 0;
-
-    char (*lines)[81] = malloc(100 * sizeof *lines);
+    char (*lines)[line_length] = malloc(100 * sizeof *lines);
     if (!lines) {
         printf("Memory allocation failed!\n");
         fclose(this_file);
         return NULL;
     }
 
-    for (int i = 0; i < 100; i++) {
-        if (fgets(lines[i], 81, this_file) == NULL) break;
+    for (int i = 0; i < max_lines; i++) {
+        if (fgets(lines[i], line_length, this_file) == NULL) break;
         lines[i][strcspn(lines[i], "\n")] = '\0';
         count++;
     }
 
     *count_out = count;
     fclose(this_file);
-    return (char*)lines;
+    return lines;
 }
 
-void convert_to_uppercase(char (*this_lines)[81], const int this_count) {
+void convert_to_uppercase(char (*this_lines)[line_length], const int this_count) {
     for (int i = 0; i < this_count; i++) {
         for (int j = 0; j < strlen(this_lines[i]); j++) {
             const char c = this_lines[i][j];
@@ -82,8 +85,7 @@ void convert_to_uppercase(char (*this_lines)[81], const int this_count) {
     }
 }
 
-
-void write_to_file(char *this_filename, char (*this_lines)[81], const int this_count) {
+void write_to_file(char *this_filename, char (*this_lines)[line_length], const int this_count) {
     FILE *file = fopen(this_filename, "w");
     if (file == NULL) {
         fprintf(stderr, "Error: could not open file: '%s'\n", this_filename);
