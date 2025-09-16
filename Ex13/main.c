7ed1917a8b794f8de04c699c3e5c17fde102dbc6
ville <ville.karppinen88@gmail.com>
2025-09-16 14:24:57 +0300

    Ex13 updated

--- Ex13/main.c
+++ Ex13/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-char *handle_input();
+#define filename_length 34
+
+char *get_filename();
+bool get_input(char *user_input);
 FILE *open_file(char *this_filename);
 int *read_file(FILE *this_file, int *count);
 void handle_print(const int *numbers, int count);
@@ -10,7 +14,7 @@ void handle_print(const int *numbers, int count);
 int main() {
     int count = 0;
 
-    char *filename = handle_input();
+    char *filename = get_filename();
     FILE *file = open_file(filename);
     int *numbers = read_file(file, &count);
     if (numbers != NULL)
@@ -22,38 +26,51 @@ int main() {
     return 0;
 }
 
-char *handle_input() {
-    char *string = malloc(32);
-    if (!string) {
-        printf("Memory allocation failed!\n");
-        return NULL;
+char *get_filename() {
+    char *user_input = malloc(filename_length);
+    if (user_input) {
+        bool stop_loop = false;
+        while (!stop_loop) {
+            printf("Enter a filename: ");
+            stop_loop = get_input(user_input);
+        }
+        return user_input;
     }
+    printf("Memory allocation failed.\n");
+    return NULL;
+}
 
-    printf("Enter a filename: ");
-    if (!fgets(string, 32, stdin)) {
-        free(string);
-        return NULL;
+bool get_input(char *user_input) {
+    if (fgets(user_input, filename_length, stdin)) {
+        if (strchr(user_input, '\n') == NULL) {
+            int c = 0;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            printf("Input too long (max %d characters).\n", filename_length-2);
+            return false;
+        }
+        user_input[strcspn(user_input, "\n")] = '\0';
+        if (user_input[0] == '\0') {
+            printf("Empty input.\n");
+            return false;
+        }
+        return true;
     }
-
-    string[strcspn(string, "\n")] = '\0';
-
-    return string;
+    return false;
 }
 
 FILE *open_file(char *this_filename) {
     FILE *file;
-
     if ((file = fopen(this_filename, "r")) == NULL) {
         fprintf(stderr, "Error: could not open file '%s'\n", this_filename);
         free(this_filename);
         exit(EXIT_FAILURE);
     }
-
     return file;
 }
 
 int *read_file(FILE *this_file, int *count) {
-    int *numbers = NULL, temp;
+    int *numbers = NULL;
+    int temp;
 
     while (fscanf(this_file, "%d", &temp) == 1) {
         int *buffer = realloc(numbers, sizeof(temp) * (*count+1));
@@ -71,7 +88,8 @@ int *read_file(FILE *this_file, int *count) {
 }
 
 void handle_print(const int *numbers, const int count) {
-    int lowest = numbers[0], highest = numbers[0];
+    int lowest = numbers[0];
+    int highest = numbers[0];
 
     for (int i = 0; i < count; i++) {
         if (numbers[i] < lowest)
