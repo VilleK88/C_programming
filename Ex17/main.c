48574f5913a7c51dcba341f95e64f95c5ce99fbe
ville <ville.karppinen88@gmail.com>
2025-09-23 13:20:00 +0300

    Ex17 updated

--- Ex17/main.c
+++ Ex17/main.c
@@ -4,8 +4,10 @@
 #include <stdbool.h>
 #include <time.h>
 
-char *handle_input();
-char *input_warning_and_free_memory(char *error_msg, char *string);
+#define input_length 34
+
+char *handle_input(int length);
+bool get_input(char *user_input, int length);
 bool password_generator(char *word_out, int array_size, const char *word_in);
 const char * const *get_table(void);
 
@@ -16,25 +18,23 @@ int main() {
     bool continue_loop = true;
 
     do {
-        printf("Enter password: ");
-        char *word = handle_input();
-        if (word != NULL) {
-            if (strcmp(word, "stop") != 0) {
-                char word_out[strlen(word)];
-                const bool successful = password_generator(word_out, 32, word);
-                if (successful) {
-                    char **tmp = realloc(passwords, (size_t)(count+1) * sizeof * passwords);
-                    if (!tmp) free(tmp);
-                    else {
-                        passwords = tmp;
-                        // allocates heap memory and copies word_out
-                        passwords[count++] = strdup(word_out);
-                        printf("Word in array: %s\n", passwords[count-1]);
-                    }
+        char *word = handle_input(input_length);
+        if (strcmp(word, "stop") != 0) {
+            char word_out[strlen(word)];
+            const bool successful = password_generator(word_out, 32, word);
+            if (successful) {
+                char **tmp = realloc(passwords, (size_t)(count+1) * sizeof * passwords);
+                if (!tmp) free(tmp);
+                else {
+                    passwords = tmp;
+                    // allocates heap memory and copies word_out
+                    passwords[count++] = strdup(word_out);
+                    printf("Word in array: %s\n", passwords[count-1]);
                 }
             }
-            else continue_loop = false;
         }
+        else continue_loop = false;
+
         free(word);
     } while (continue_loop);
 
@@ -42,33 +42,36 @@ int main() {
     return 0;
 }
 
-char *handle_input() {
-    char *string = malloc(32);
-
+char *handle_input(const int length) {
+    char *string = malloc(length);
     if (string) {
-        if (fgets(string, 32, stdin)) {
-            if (strchr(string, '\n') == NULL) {
-                int ch;
-                while ((ch = getchar()) != '\n' && ch != EOF){}
-                return input_warning_and_free_memory("Input too long (max 32 characters).\n", string);
-            }
-            // replaces the first '\n' in the string with '\0' to remove the newline
-            string[strcspn(string, "\n")] = '\0';
-
-            if (string[0] == '\0')
-                return input_warning_and_free_memory("Empty input.\n", string);
+        bool stop_loop = false;
+        while (!stop_loop) {
+            printf("Enter password: ");
+            stop_loop = get_input(string, length);
         }
+        return string;
     }
-    else
-        return input_warning_and_free_memory("Memory allocation failed.\n", string);
-
-    return string;
+    printf("Memory allocation failed.\n");
+    exit(EXIT_FAILURE);
 }
 
-char *input_warning_and_free_memory(char *error_msg, char *string) {
-    printf("%s", error_msg);
-    free(string);
-    return NULL;
+bool get_input(char *user_input, const int length) {
+    if(fgets(user_input, length, stdin)) {
+        if (strchr(user_input, '\n') == NULL) {
+            int c = 0;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            printf("Input too long (max %d characters).\n", length-2);
+            return false;
+        }
+        user_input[strcspn(user_input, "\n")] = '\0';
+        if (user_input[0] == '\0') {
+            printf("Empty input.\n");
+            return false;
+        }
+        return true;
+    }
+    return false;
 }
 
 bool password_generator(char *word_out, int const array_size, const char *word_in) {
