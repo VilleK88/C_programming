c13bbbb5f8f134ce80a39b12ccc9529f4e8a5fa1
ville <ville.karppinen88@gmail.com>
2025-10-09 10:40:25 +0300

    Ex20 updated

--- Ex20/debug.c
+++ Ex20/debug.c
@@ -10,16 +10,16 @@ void set_debug_level(const int debug_level) {
 
 int dprintf(const int debug_level, const char *fmt, ...) {
     if (debug_level <= d_debug_level) {
-        int written = 0;
-        written += fprintf(stderr, "[DBG%d] ", debug_level);
+        int standard_error = 0;
+        standard_error += fprintf(stderr, "[DBG%d] ", debug_level);
 
         va_list ap;
         va_start(ap, fmt);
-        written += vfprintf(stderr, fmt, ap);
+        standard_error += vfprintf(stderr, fmt, ap);
         va_end(ap);
 
-        written += fputc('\n', stderr);
-        return written;
+        standard_error += fputc('\n', stderr);
+        return standard_error;
     }
 
     return 0;
