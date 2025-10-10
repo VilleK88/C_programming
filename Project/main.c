c1fd3bffb21d766878b95cadb75198a319be502b
ville <ville.karppinen88@gmail.com>
2025-10-10 14:04:46 +0300

    Project updated

--- Project/main.c
+++ Project/main.c
@@ -185,23 +185,6 @@ void add_passenger(const char *first_name, const char *last_name, const int row,
     printf("Passenger added\n");
 }
 
-/*bool needs_line_break() {
-    FILE *file = open_file("seat_reservations.csv", "rb"); // open file in binary mode
-    // check if the file is empty
-    const bool empty_file = fseek(file, 0, SEEK_END) == 0 &&  ftell(file) == 0;
-    if (!empty_file) {
-        const int line_feed = '\x0A'; // represents '\n'
-        // determine if a newline character is present if file is not empty
-        const bool result = fseek(file, -1, SEEK_END) == 0 && fgetc(file) == line_feed;
-        if (!result) {
-            fclose(file);
-            return true;
-        }
-    }
-    fclose(file);
-    return false;
-}*/
-
 void show_passengers() {
     struct passenger_ passengers[156];
     int count = 0;
