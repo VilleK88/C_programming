5b7568ac9a9bb4c19bcd929980b7474ae4e6734f
ville <ville.karppinen88@gmail.com>
2025-10-15 15:19:39 +0300

    VAaCP3 done

--- VAaCP3/main.c
+++ VAaCP3/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
 
 typedef struct {
     const char *text;
@@ -16,14 +15,12 @@ int main() {
     text_reader_init(&ctx, text);
 
     char buffer[64];
-    /*while (true) {
+    while (true) {
         const int n = text_reader_read(&ctx, buffer, sizeof buffer);
         if (n == 0)
             break;
-        printf("Luettiin: %s\n", buffer);
-    }*/
-    const int n = text_reader_read(&ctx, buffer, sizeof buffer);
-    printf("Luettiin: %s\n", buffer);
+        printf("Luettiin: %s", buffer);
+    }
 
     return 0;
 }
@@ -34,20 +31,27 @@ void text_reader_init(text_reader_ctx *ctx, const char *str) {
 }
 
 int text_reader_read(text_reader_ctx *ctx, char *buffer, int size) {
-    int index = 0;
+    int count = 0;
     bool continue_loop = true;
 
     do {
         char c = ctx->text[ctx->position];
-        buffer[index++] = c;
-        ctx->position++;
-        if (c == '\0')
-            continue_loop = false;
-        if (c == '\n') {
-            ctx->position++;
-            continue_loop = false;
+        if (c != '\0' && count < size - 1) {
+            if (c != '\n') {
+                buffer[count++] = c;
+                ctx->position++;
+            }
+            else {
+                buffer[count++] = c;
+                ctx->position++;
+                continue_loop = false;
+            }
         }
+        else
+            continue_loop = false;
+
     } while (continue_loop);
-    buffer[index] = '\0';
-    return index;
+
+    buffer[count] = '\0';
+    return count;
 }
