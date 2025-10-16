fdc70487d9643a0a1fe9c0e7e79a367ea3df91d5
ville <ville.karppinen88@gmail.com>
2025-10-16 07:43:25 +0300

    FP4 updated

---
+++ FP4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define SLIP_END 0xC0
+#define SLIP_ESC 0xDB
+#define SLIP_ESC_END 0xDC
+#define SLIP_ESC_ESC 0xDD
+
+int main() {
+    return 0;
+}
